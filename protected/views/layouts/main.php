68196b756497c19206bf777a5bad238ce9e413a4
Sasha Kosyanchuk <dedanaan00@gmail.com>
2017-02-23 00:35:07 +0200

    registerCoreScript

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,4 +1,9 @@
 <?php /* @var $this Controller */ ?>
+
+<?php
+	Yii::app()->clientScript->registerCoreScript('jquery');
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
@@ -16,7 +21,7 @@
 
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-	
+
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 

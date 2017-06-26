93dbeb0c63153caee49eab663d34027f37f070b2
AlexanderKosianchuk <alexanderkosianchuk@luch15.com>
2017-06-26 23:17:38 +0300

    get folder content via relations write

--- protected/controllers/AdminController.php
+++ protected/controllers/AdminController.php
@@ -55,143 +55,68 @@ class AdminController extends Controller
             exit;
         }
 
-        $connection = Yii::app()->db;
-        $connection->active=true;
-        $userName = Yii::app()->user->name;
+        $userId = Yii::app()->user->id;
         $type = $_GET['type'];
 
-        $folderid = $_GET['id'];
-        $folderName = "Root node";
-        if ($folderid == '#') {
-            $folderid = 0;
+        $folderId = 0;
 
-            if ($type == 'treeGeneral') {
-                $folderName = "General content";
-            } else if($type == 'treePrivate') {
-                $folderName = "Private content";
-            }
-        } else {
-            $sql = "SELECT `name` FROM `folder` WHERE `id` = " . $folderid . ";";
-            $command = $connection->createCommand($sql);
-            $dataReader=$command->query();
-
-            if(($row=$dataReader->read())!==false) {
-                $folderName = $row['name'];
-            }
+        if ($_GET['id'] !== '#') {
+            $folderId = intval($_GET['id']);
         }
-
-        $tree = array();
 
         if ($type == 'treeGeneral') {
-            $adminName = 'admin';
-            $sql = "SELECT `id`, `name`, `path` FROM `folder` WHERE `author` = '".$adminName."';";
-            $command = $connection->createCommand($sql);
-            $dataReader=$command->query();
-            $relatedNodes = array();
-            $d = array();
-
-            while (($row=$dataReader->read())!==false) {
-                $d[] = array(
-                        'id' => intval($row['id']),
-                        'text' => $row['name'],
-                        'type' => 'folder',
-                        'parent' => intval($row['path']),
-                );
-            }
-
-            $sql = "SELECT `id_file`, `id_folder` FROM `file_to_folder` WHERE `id_author` = '".$adminName."';";
-            $command = $connection->createCommand($sql);
-            $dataReader=$command->query();
-
-            while (($row=$dataReader->read())!==false) {
-                $sql = "SELECT `name` FROM `file` WHERE `id` = ".$row['id_file'].";";
-                $command = $connection->createCommand($sql);
-                $dataReader1=$command->query();
-
-                if (($row1 = $dataReader1->read()) !== false) {
-                    $name = substr($row1['name'], 13, strlen($row1['name']) - 13);
-                    $d[] = array(
-                            'id' => intval($row['id_file']),
-                            'text' => $name,
-                            'type' => 'file',
-                            'parent' => intval($row['id_folder']),
-                    );
-                }
-            }
-
-            if (count($d) > 0) {
-                $relatedNodes = $this->makeRecursive($d);
-            } else {
-                $relatedNodes = false;
-            }
-
-            $connection->active=false;
-            $tree[] = array(
-                "id" => (string)$folderid,
-                "text" => $folderName,
-                "state" => array(
-                    "opened" => true
-                ),
-                'type' => 'folder',
-                'children' => $relatedNodes
-            );
-
-        } else if ($type == 'treePrivate') {
-            $sql = "SELECT `id`, `name`, `path` FROM `folder` WHERE `author` = '".$userName."';";
-            $command = $connection->createCommand($sql);
-            $dataReader=$command->query();
-            $relatedNodes = array();
-            $d = array();
-
-            while(($row = $dataReader->read()) !== false) {
-                $d[] = array(
-                    'id' => intval($row['id']),
-                    'text' => $row['name'],
-                    'type' => 'folder',
-                    'parent' => intval($row['path']),
-                );
-            }
-
-            $sql = "SELECT `id_file`, `id_folder` FROM `file_to_folder` WHERE `id_author` = '".$userName."';";
-            $command = $connection->createCommand($sql);
-            $dataReader=$command->query();
-
-            while(($row = $dataReader->read()) !== false) {
-                $sql = "SELECT `name` FROM `file` WHERE `id` = ".$row['id_file'].";";
-                $command = $connection->createCommand($sql);
-                $dataReader1=$command->query();
-
-                if(($row1 = $dataReader1->read()) !== false) {
-                    $name = substr($row1['name'], 13, strlen($row1['name']) - 13);
-                    $d[] = array(
-                        'id' => intval($row['id_file']),
-                        'text' => $name,
-                        'type' => 'file',
-                        'parent' => intval($row['id_folder']),
-                    );
-                }
-            }
-
-            if(count($d) > 0) {
-                $relatedNodes = $this->makeRecursive($d);
-            } else {
-                $relatedNodes = false;
-            }
-
-            $connection->active=false;
-            $tree[] = array(
-                "id" => (string)$folderid,
-                "text" => $folderName,
-                "state" => array(
-                    "opened" => true,
-                    "selected" => true
-                ),
-                'type' => 'folder',
-                'children' => $relatedNodes
-            );
+            $folderName = "General content";
+        } else if($type == 'treePrivate') {
+            $folderName = "Private content";
         }
 
-        //error_log(json_encode($tree));
+        $tree = [];
+
+        $author = 'admin';
+        if ($type == 'treePrivate') {
+            $author = Yii::app()->user->username;
+        }
+
+        $folders = Folder::model()->findAllByAttributes(['author' => $author]);
+
+        $d = [];
+
+        foreach ($folders as $folder) {
+            $d[] = array(
+                    'id' => intval($folder->id),
+                    'text' => $folder->name,
+                    'type' => 'folder',
+                    'parent' => intval($folder->path),
+            );
+
+            $files = $folder->files;
+            foreach ($files as $file) {
+                $name = substr($file->name, 13, strlen($file->name) - 13);
+                $d[] = array(
+                        'id' => intval($file->id),
+                        'text' => $name,
+                        'type' => 'file',
+                        'parent' => intval($folder->id),
+                );
+            }
+        }
+
+        if (count($d) > 0) {
+            $relatedNodes = $this->makeRecursive($d);
+        } else {
+            $relatedNodes = false;
+        }
+
+        $tree[] = array(
+            "id" => (string)$folderId,
+            "text" => $folderName,
+            "state" => array(
+                "opened" => true
+            ),
+            'type' => 'folder',
+            'children' => $relatedNodes
+        );
+
         echo json_encode($tree);
     }
 
---
+++ protected/models/Folder.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * This is the model class for table "folder".
+ *
+ * The followings are the available columns in table 'folder':
+ * @property integer $id
+ * @property string $name
+ * @property integer $path
+ * @property string $author
+ */
+class Folder extends CActiveRecord
+{
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'folder';
+    }
+
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        return array(
+            array('name, path, author', 'required'),
+            array('path', 'numerical', 'integerOnly'=>true),
+            array('name, author', 'length', 'max'=>255),
+            array('name', 'safe', 'on'=>'search'),
+        );
+    }
+
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        return array(
+            'fileToFolder' => array(self::HAS_MANY, 'FileToFolder', 'id_folder'),
+            'files'=>array(self::HAS_MANY, 'File', ['id_file'=>'id'],'through'=>'fileToFolder'),
+        );
+    }
+
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+            'id' => 'ID',
+            'name' => 'Name',
+            'path' => 'Path',
+            'author' => 'Author',
+        );
+    }
+
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     *
+     * Typical usecase:
+     * - Initialize the model fields with values from filter form.
+     * - Execute this method to get CActiveDataProvider instance which will filter
+     * models according to data in model fields.
+     * - Pass data provider to CGridView, CListView or any similar widget.
+     *
+     * @return CActiveDataProvider the data provider that can return the models
+     * based on the search/filter conditions.
+     */
+    public function search()
+    {
+        // @todo Please modify the following code to remove attributes that should not be searched.
+
+        $criteria=new CDbCriteria;
+
+        $criteria->compare('id',$this->id);
+        $criteria->compare('name',$this->name,true);
+        $criteria->compare('path',$this->path);
+        $criteria->compare('author',$this->author,true);
+
+        return new CActiveDataProvider($this, array(
+            'criteria'=>$criteria,
+        ));
+    }
+
+    /**
+     * Returns the static model of the specified AR class.
+     * Please note that you should have this exact method in all your CActiveRecord descendants!
+     * @param string $className active record class name.
+     * @return Folder the static model class
+     */
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+    }
+}
